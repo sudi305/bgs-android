d9d17ae3673b27feb04ee539b694a3cbdd3bc473
sudi305 <sudiyanto@bening-semesta.com>
2016-01-15 15:26:12 +0700

    Dheket - Login Fb example

---
+++ app/src/main/java/com/bgs/dheket/FormLogin.java
@@ -0,0 +1,13 @@
+package com.bgs.dheket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+    }
+}
