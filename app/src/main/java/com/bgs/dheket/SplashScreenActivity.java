c32bee1168288358e761112112acedd93485e7ad
sudi305 <sudiyanto@bening-semesta.com>
2016-01-19 18:22:01 +0700

    Class roundedButton untuk tombol lingkaran Instalasi ArcGis ke project dheket

--- app/src/main/java/com/bgs/dheket/SplashScreenActivity.java
+++ app/src/main/java/com/bgs/dheket/SplashScreenActivity.java
@@ -2,9 +2,13 @@ package com.bgs.dheket;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
+import com.bgs.common.RoundImage;
 import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 
@@ -14,12 +18,19 @@ import com.facebook.FacebookSdk;
 public class SplashScreenActivity extends Activity{
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 3000;
+    ImageView imgLogo;
+    RoundImage crop_image_circle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        imgLogo = (ImageView)findViewById(R.id.imgLogo);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.logo);
+        crop_image_circle = new RoundImage(bitmap);
+        imgLogo.setImageDrawable(crop_image_circle);
+        imgLogo.setScaleType(ImageView.ScaleType.CENTER);
 
         new Handler().postDelayed(new Runnable() {
             @Override
