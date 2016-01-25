a585caa7ddeba75f66df9c91a66d074c4e5e8771
sudi305 <sudiyanto@bening-semesta.com>
2016-01-25 20:52:29 +0700

    Checking connection and gps Retrieving data from web service (example) Resizing buble with total location each category animating layout main menu

---
+++ app/src/main/java/com/bgs/common/Dialogs.java
@@ -0,0 +1,50 @@
+package com.bgs.common;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import com.bgs.dheket.R;
+
+/**
+ * Created by SND on 21/01/2016.
+ */
+public class Dialogs extends DialogFragment {
+    static int message=0;
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder dshow = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        if (message==1)dshow.setMessage("cek");
+        dshow.setTitle("Alert");
+        dshow.setView(inflater.inflate(R.layout.dialogconc, null))
+
+                .setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        //TODO
+
+
+                    }
+                })
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Dialogs.this.getDialog().cancel();
+                    }
+                });
+
+        return dshow.create();
+    }
+
+    public static Dialogs newInstance(int msg){
+        message = msg;
+        Dialogs arg = new Dialogs();
+        return arg;
+    }
+}
--- app/src/main/java/com/bgs/dheket/MainMenuActivity.java
+++ app/src/main/java/com/bgs/dheket/MainMenuActivity.java
@@ -2,21 +2,36 @@ package com.bgs.dheket;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bgs.common.ConfigInternetAndGPS;
+import com.bgs.common.Dialogs;
 import com.bgs.common.ProfilePictureView_viaFB;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -25,23 +40,46 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by SND on 20/01/2016.
  */
 
-public class MainMenuActivity extends AppCompatActivity {
-    LinearLayout buble_rest,buble_salon,buble_atm,buble_apotek,buble_mart;
-    Button btn_buble_rest,btn_buble_salon,btn_buble_atm,btn_buble_apotek,btn_buble_mart,btn_search;
-    TextView txt_tot_rest,txt_tot_salon,txt_tot_atm,txt_tot_apotek,txt_tot_mart;
-    TextView txt_promo_rest,txt_promo_salon,txt_promo_atm,txt_promo_apotek,txt_promo_mart;
+public class MainMenuActivity extends AppCompatActivity implements LocationListener{
+
+    LinearLayout buble_cat1,buble_cat2,buble_cat3,buble_cat4,buble_cat5;
+    Button btn_buble_cat1,btn_buble_cat2,btn_buble_cat3,btn_buble_cat4,btn_buble_cat5,btn_search;
+    TextView txt_tot_cat1,txt_tot_cat2,txt_tot_cat3,txt_tot_cat4,txt_tot_cat5;
+    TextView txt_promo_cat1,txt_promo_cat2,txt_promo_cat3,txt_promo_cat4,txt_promo_cat5;
     ImageView imVi_usrPro;
     ProfilePictureView_viaFB view_usrPro;
     CallbackManager callbackManager;
     Dialog details_dialog;
     TextView details_txt,textView_usrNm;
+    ConfigInternetAndGPS checkInternetGPS;
+
+    private JSONObject jObject;
+    private String jsonResult ="";
+    int[] promo=new int[5], lokasi =new int[5], id_kategori=new int[5];
+    String[] nama_katagori=new String[5];
+    double radius = 0.0;
+    double latitude, longitude;
+    String url = "http://dheket.esy.es/getLocationPromo.php";
+
+    boolean tambah = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,29 +88,30 @@ public class MainMenuActivity extends AppCompatActivity {
 
         FacebookSdk.sdkInitialize(getApplicationContext());
 
-        buble_apotek = (LinearLayout)findViewById(R.id.buble_apotek);
-        buble_salon = (LinearLayout)findViewById(R.id.buble_salon);
-        buble_rest = (LinearLayout)findViewById(R.id.buble_rest);
-        buble_mart = (LinearLayout)findViewById(R.id.buble_mart);
-        buble_atm = (LinearLayout)findViewById(R.id.buble_atm);
-        btn_buble_apotek = (Button)findViewById(R.id.btn_buble_apotek);
-        btn_buble_salon = (Button)findViewById(R.id.btn_buble_salon);
-        btn_buble_rest = (Button)findViewById(R.id.btn_buble_rest);
-        btn_buble_mart = (Button)findViewById(R.id.btn_buble_mart);
-        btn_buble_atm = (Button)findViewById(R.id.btn_buble_atm);
+        checkInternetGPS = new ConfigInternetAndGPS(getApplicationContext());
+
+        buble_cat1 = (LinearLayout)findViewById(R.id.buble_cat1);
+        buble_cat2 = (LinearLayout)findViewById(R.id.buble_cat2);
+        buble_cat3 = (LinearLayout)findViewById(R.id.buble_cat3);
+        buble_cat4 = (LinearLayout)findViewById(R.id.buble_cat4);
+        buble_cat5 = (LinearLayout)findViewById(R.id.buble_cat5);
+        btn_buble_cat1 = (Button)findViewById(R.id.btn_buble_cat1);
+        btn_buble_cat2 = (Button)findViewById(R.id.btn_buble_cat2);
+        btn_buble_cat3 = (Button)findViewById(R.id.btn_buble_cat3);
+        btn_buble_cat4 = (Button)findViewById(R.id.btn_buble_cat4);
+        btn_buble_cat5 = (Button)findViewById(R.id.btn_buble_cat5);
         btn_search = (Button)findViewById(R.id.btn_search);
         imVi_usrPro = (ImageView)findViewById(R.id.imageView_userProfile);
-        txt_promo_apotek = (TextView)findViewById(R.id.textView_promo_apotek);
-        txt_promo_atm = (TextView)findViewById(R.id.textView_promo_atm);
-        txt_promo_mart = (TextView)findViewById(R.id.textView_promo_mart);
-        txt_promo_rest = (TextView)findViewById(R.id.textView_promo_rest);
-        txt_promo_salon = (TextView)findViewById(R.id.textView_promo_salon);
-        txt_tot_apotek = (TextView)findViewById(R.id.textView_total_apotek);
-        txt_tot_atm = (TextView)findViewById(R.id.textView_total_atm);
-        txt_tot_mart = (TextView)findViewById(R.id.textView_total_mart);
-        txt_tot_rest = (TextView)findViewById(R.id.textView_total_rest);
-        txt_tot_salon = (TextView)findViewById(R.id.textView_total_salon);
-        txt_tot_salon = (TextView)findViewById(R.id.textView_total_salon);
+        txt_promo_cat1 = (TextView)findViewById(R.id.textView_promo_cat1);
+        txt_promo_cat2 = (TextView)findViewById(R.id.textView_promo_cat2);
+        txt_promo_cat3 = (TextView)findViewById(R.id.textView_promo_cat3);
+        txt_promo_cat4 = (TextView)findViewById(R.id.textView_promo_cat4);
+        txt_promo_cat5 = (TextView)findViewById(R.id.textView_promo_cat5);
+        txt_tot_cat1 = (TextView)findViewById(R.id.textView_total_cat1);
+        txt_tot_cat2 = (TextView)findViewById(R.id.textView_total_cat2);
+        txt_tot_cat3 = (TextView)findViewById(R.id.textView_total_cat3);
+        txt_tot_cat4 = (TextView)findViewById(R.id.textView_total_cat4);
+        txt_tot_cat5 = (TextView)findViewById(R.id.textView_total_cat5);
         view_usrPro = (ProfilePictureView_viaFB)findViewById(R.id.view_userProfile);
         details_dialog = new Dialog(this);
         details_dialog.setContentView(R.layout.dialog_details);
@@ -80,20 +119,33 @@ public class MainMenuActivity extends AppCompatActivity {
         details_txt = (TextView)details_dialog.findViewById(R.id.details);
         textView_usrNm = (TextView)findViewById(R.id.textView_usrNm);
 
+        LocationManager myLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = myLocationManager.getBestProvider(criteria, true);
+        Location location = myLocationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+        myLocationManager.requestLocationUpdates(provider, 20000, 0, this);
+
+
         final Animation animTranslate = AnimationUtils.loadAnimation(this, R.anim.anim_translate);
-        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
         final Animation animScale = AnimationUtils.loadAnimation(this, R.anim.anim_scale);
         final Animation animButtonPress = AnimationUtils.loadAnimation(this, R.anim.anim_scale_button_press);
 
         imVi_usrPro.setAnimation(animScale);
-        buble_apotek.setAnimation(animTranslate);
-        buble_salon.setAnimation(animTranslate);
-        buble_rest.setAnimation(animTranslate);
-        buble_mart.setAnimation(animTranslate);
-        buble_atm.setAnimation(animTranslate);
+        buble_cat1.setAnimation(animTranslate);
+        buble_cat2.setAnimation(animTranslate);
+        buble_cat3.setAnimation(animTranslate);
+        buble_cat4.setAnimation(animTranslate);
+        buble_cat5.setAnimation(animTranslate);
+
+//        FragmentManager manager = getSupportFragmentManager(); // or getFragmentManager, depends on which api lvl you are working on but supportFragmentManager will make you dialog work also on devices lower than api lvl 11(3.0 - > Honeycomb)
+//        DialogFragment Dialog = Dialogs.newInstance(1);
+//        Dialog.show(getFragmentManager(), "tag");
 
         if(AccessToken.getCurrentAccessToken() != null){
-            RequestData();
+            RequestDataFromFB();
         }
 
         view_usrPro.setOnClickListener(new View.OnClickListener() {
@@ -103,38 +155,38 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
-        btn_buble_apotek.setOnClickListener(new View.OnClickListener() {
+        btn_buble_cat1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn_buble_apotek.setAnimation(animAlpha);
+
             }
         });
 
-        btn_buble_salon.setOnClickListener(new View.OnClickListener() {
+        btn_buble_cat2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn_buble_salon.setAnimation(animAlpha);
+
             }
         });
 
-        btn_buble_rest.setOnClickListener(new View.OnClickListener() {
+        btn_buble_cat3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn_buble_rest.setAnimation(animAlpha);
+
             }
         });
 
-        btn_buble_mart.setOnClickListener(new View.OnClickListener() {
+        btn_buble_cat4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn_buble_mart.setAnimation(animAlpha);
+
             }
         });
 
-        btn_buble_atm.setOnClickListener(new View.OnClickListener() {
+        btn_buble_cat5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn_buble_atm.setAnimation(animAlpha);
+
             }
         });
 
@@ -142,11 +194,14 @@ public class MainMenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 btn_search.setAnimation(animButtonPress);
+                btn_search.setText("Internet "+checkInternetGPS.isConnectingToInternet()+" | GPS "+checkInternetGPS.isGPSActived());
+                if (tambah==true)tambah=false;
+                else tambah=true;
             }
         });
     }
 
-    public void RequestData(){
+    public void RequestDataFromFB(){
         GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
             @Override
             public void onCompleted(JSONObject object,GraphResponse response) {
@@ -212,5 +267,142 @@ public class MainMenuActivity extends AppCompatActivity {
                 })
                 .setNegativeButton(cancel, null);
         builder.create().show();
+    }
+
+    public void resizeBuble(Button button, int lokasi){
+        if (lokasi<=5)lokasi=100;
+        else if (lokasi>5 && lokasi<11) lokasi=lokasi*20;
+        else if (lokasi>10)lokasi=200;
+        ViewGroup.LayoutParams params = button.getLayoutParams();
+        params.width=lokasi;
+        params.height=lokasi;
+        button.setLayoutParams(params);
+    }
+
+    /**
+     * Method untuk Mengirimkan data ke server
+     */
+    public String getRequest(String Url){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String sreq="";
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(Url);
+        try{
+            HttpResponse response = client.execute(request);
+            sreq =setRequest(response);
+
+        }catch(Exception ex){
+            Toast.makeText(this,"Gagal "+sreq, Toast.LENGTH_SHORT).show();
+            Log.e("GAGAL",""+ex);
+        }
+        return sreq;
+    }
+
+    /**
+     * Method untuk Menerima data dari server
+     */
+    public static String setRequest(HttpResponse response){
+        String result = "";
+        try{
+            InputStream in = response.getEntity().getContent();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder str = new StringBuilder();
+            String line = null;
+            while((line = reader.readLine()) != null){
+                str.append(line + "\n");
+            }
+            in.close();
+            result = str.toString();
+        }catch(Exception ex){
+            result = "Error";
+        }
+
+        return result;
+    }
+
+    public void getDataCategory(double rad,double lat, double lng) {
+        Log.e("DHEKET ", url+"?rad="+rad+"&lat="+lat+"&lng="+lng);
+        try {
+            jsonResult = getRequest(url+"?rad="+rad+"&lat="+lat+"&lng="+lng);
+            jObject = new JSONObject(jsonResult);
+            JSONArray menuItemArray = jObject.getJSONArray("dheket_totLocation");
+            for (int i = 0; i < menuItemArray.length(); i++) {
+                id_kategori[i] = menuItemArray.getJSONObject(i).getInt("id_category");
+                nama_katagori[i] = menuItemArray.getJSONObject(i).getString("category_name").toString();
+                lokasi[i] = menuItemArray.getJSONObject(i).getInt("total_location");
+                promo[i] = menuItemArray.getJSONObject(i).getInt("total_promo");
+            }
+
+            Log.e("cek","SUKSES"+nama_katagori.length+"|");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e("cek","Error");
+        }
+    }
+
+    public void updateData(){
+        txt_tot_cat1.setText(""+lokasi[0]);
+        String cat1 = "-";
+        if (nama_katagori[0]!=null) cat1=nama_katagori[0];
+        btn_buble_cat1.setText(""+cat1);
+        txt_promo_cat1.setText("Promo: "+promo[0]);
+        resizeBuble(btn_buble_cat1, lokasi[0]);
+
+        txt_tot_cat2.setText("" + lokasi[1]);
+        String cat2 = "-";
+        if (nama_katagori[1]!=null) cat2=nama_katagori[1];
+        btn_buble_cat2.setText(""+cat2);
+        txt_promo_cat2.setText("Promo: "+promo[1]);
+        resizeBuble(btn_buble_cat2, lokasi[1]);
+
+        txt_tot_cat3.setText("" + lokasi[2]);
+        String cat3 = "-";
+        if (nama_katagori[2]!=null) cat3=nama_katagori[2];
+        btn_buble_cat3.setText(""+cat3);
+        txt_promo_cat3.setText("Promo: "+promo[2]);
+        resizeBuble(btn_buble_cat3, lokasi[2]);
+
+        txt_tot_cat4.setText("" + lokasi[3]);
+        String cat4 = "-";
+        if (nama_katagori[3]!=null) cat4=nama_katagori[3];
+        btn_buble_cat4.setText(""+cat4);
+        txt_promo_cat4.setText("Promo: "+promo[3]);
+        resizeBuble(btn_buble_cat4, lokasi[3]);
+
+        txt_tot_cat5.setText("" + lokasi[4]);
+        String cat5 = "-";
+        if (nama_katagori[4]!=null) cat5=nama_katagori[4];
+        btn_buble_cat5.setText(""+cat5);
+        txt_promo_cat5.setText("Promo: "+promo[4]);
+        resizeBuble(btn_buble_cat5,lokasi[4]);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        Toast.makeText(getApplicationContext(),"lat "+latitude+" | lgt "+longitude, Toast.LENGTH_LONG).show();
+        getDataCategory(radius, latitude, longitude);
+        if (tambah==true)radius=radius+0.4;
+        else radius=radius-0.4;
+        updateData();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        String message = "GPS enabled";
+        Toast.makeText(getApplicationContext(),message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        String message = "GPS disabled";
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 }
